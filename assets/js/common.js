24f2ee2c942a0692e9104e9ebdd57536018588e2
nguyentungdiep.fit@gmail.com <rachel1234>
2017-11-17 16:54:25 +0700

    Giao diện
    Thêm giao diện của các trang sản phẩm, giới thiệu, liên hệ

---
+++ assets/js/common.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+	//------------------------------
+	$(".single-item").slick({
+	    dots: true,
+		autoplay: true,
+	});
+});
+$( function() {
+    $( "#tabs" ).tabs();
+});
+// $(document).ready(function () {
+//   //------------------------------
+//   $(".single-section").slick({
+//       dots: true,
+//       autoplay: true,
+//   });
+//   });
+
+
+// $(".buttom--toggle").click(function(){
+//    $(".more__info").toggleClass("show");
+// });
+
+// $(".menu-btn").click(function(){
+//    $(".header-down").toggleClass("show-1");
+// });
+
+// $(function() {
+// $( "#tabs" ).tabs();
+// });
+
+// $(".tab--title li").click(function() {
+//   $(".tab--title li.active").removeClass("active");
+//   $(this).addClass("active");
+// });
+
+// $(document).ready(function(){
+//   $("a").on('click', function(event) {
+//     if (this.hash !== "") {
+//       event.preventDefault();
+//       var hash = this.hash;
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
+//         window.location.hash = hash;
+//       });
+//     } 
+//   });
+// });
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("btnBack").style.display = "block";
+//     } else {
+//         document.getElementById("btnBack").style.display = "none";
+//     }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+// <button onclick="topFunction()" id="btnBack" title="Go to top"><img src="assets/img/icon_BackToTop.jpg"/></button>
+
+
+//  $(document).ready(function() { 
+//     $(window).resize(function(){
+//       var width = $(window).width();
+//       if(width <= 767){
+//           $('.sec2-down').addClass('single-section');
+//       }
+// .resize();
+// });
+
